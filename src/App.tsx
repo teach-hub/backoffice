91be29a8f26960ed1623bbce3629e2b3b6c5f3fe
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2022-10-29 18:21:40 -0300

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,22 @@ import buildGraphQLProvider from 'ra-data-graphql-simple';
 
 import Dashboard from './Dashboard';
 import authProvider from './auth/authProvider';
-import { SubjectsList } from './resources/subject';
+import { ListSubjects, CreateSubject } from './resources/subject';
 
-/**
- * Query a data provider and return a promise for a response
- *
- * @param {string} type Request type, e.g GET_LIST
- * @param {string} resource Resource name, e.g. "posts"
- * @param {Object} payload Request parameters. Depends on the action type
- * @returns {Promise} the Promise for a response
- */
+const CORE_SERVICE_ADMIN_ENDPOINT = 'http://localhost:4000/admin/graphql';
 
 const App = () => {
 
     const [dataProvider, setDataProvider] = React.useState<any>(null);
 
     React.useEffect(() => {
-        buildGraphQLProvider({ clientOptions: { uri: 'http://localhost:4000/admin/graphql' } })
+        buildGraphQLProvider({ clientOptions: { uri: CORE_SERVICE_ADMIN_ENDPOINT } })
             .then(graphQlDataProvider => setDataProvider(() => graphQlDataProvider));
     }, []);
 
-    if (!dataProvider) {
-      return <h1> Cargando </h1>
-    }
-
     return (
       <Admin dataProvider={dataProvider} dashboard={Dashboard} authProvider={authProvider}>
-        <Resource name="Subject" list={SubjectsList} />
+        <Resource options={{ label: "Materias" }} name="Subject" list={ListSubjects} create={CreateSubject} />
       </Admin>
     );
 };
--- src/resources/subject.tsx
+++ src/resources/subject.tsx
@@ -1,48 +1,26 @@
 import {
-    Create, Datagrid, Edit, EditButton, List, ReferenceField,
-    ReferenceInput, SimpleForm, TextField, TextInput, useRecordContext
+  Create,
+  Datagrid,
+  List,
+  SimpleForm,
+  TextField,
+  TextInput,
 } from 'react-admin';
 
-const PostTitle = () => {
-    const record = useRecordContext();
-    return <span>{record ? `Editar ${record.title}` : ''}</span>;
-};
-
-const postFilters = [
-    <TextInput source="q" label="Buscar" alwaysOn />,
-    <ReferenceInput source="userId" label="Usuario" reference="users" />
-];
-
-export const SubjectsList = () => (
-    <List>
-        <Datagrid>
-            {
-              // <TextField source="id" />
-              // <ReferenceField source="userId" reference="users" />
-            }
-            <TextField source="code" />
-            <TextField source="name" />
-        </Datagrid>
-    </List>
+export const ListSubjects = () => (
+  <List>
+    <Datagrid>
+      <TextField label="Codigo" source="code" />
+      <TextField label="Nombre" source="name" />
+    </Datagrid>
+  </List>
 );
 
-export const PostEdit = () => (
-    <Edit title={<PostTitle />}>
-        <SimpleForm>
-            <TextInput disabled source="id"/>
-            <ReferenceInput source="userId" reference="users" />
-            <TextInput source="title" />
-            <TextInput multiline source="body" />
-        </SimpleForm>
-    </Edit>
-);
-
-export const PostCreate = () => (
-    <Create title="Nuevo Post">
-        <SimpleForm>
-            <ReferenceInput source="userId" reference="users" />
-            <TextInput source="title" />
-            <TextInput multiline source="body" />
-        </SimpleForm>
-    </Create>
+export const CreateSubject = () => (
+  <Create title="Nueva materia">
+    <SimpleForm>
+      <TextInput label="Codigo" source="code" />
+      <TextInput label="Nombre" multiline source="name" />
+    </SimpleForm>
+  </Create>
 );
