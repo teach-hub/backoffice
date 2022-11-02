e654a24f6d23e54da77864ee2af4f3cccfb7ed39
Tomás Lopez Hidalgo <tflopezhidalgo@gmail.com>
2022-11-02 00:29:09 -0300

    Edit it

--- src/resources/subject.tsx
+++ src/resources/subject.tsx
@@ -2,16 +2,20 @@ import {
   Create,
   Datagrid,
   List,
+  Edit,
   SimpleForm,
   TextField,
   TextInput,
+  EditButton,
 } from 'react-admin';
 
 export const ListSubjects = () => (
   <List>
     <Datagrid>
+      <TextField label="id" source="id" />
       <TextField label="Codigo" source="code" />
       <TextField label="Nombre" source="name" />
+      <EditButton />
     </Datagrid>
   </List>
 );
@@ -23,4 +27,14 @@ export const CreateSubject = () => (
       <TextInput label="Nombre" multiline source="name" />
     </SimpleForm>
   </Create>
+);
+
+export const EditSubject = () => (
+  <Edit>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <TextInput source="code" />
+      <TextInput source="name" />
+    </SimpleForm>
+  </Edit>
 );
