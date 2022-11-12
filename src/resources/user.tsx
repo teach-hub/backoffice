b28f7ca15447d25797c73db8ed9e23769914e851
Tomás López Hidalgo <tflopezhidalgo@gmail.com>
2022-11-12 20:10:06 -0300

    Agrega recurso usuario (#7)

---
+++ src/resources/user.tsx
@@ -0,0 +1,62 @@
+import {
+  Create,
+  Datagrid,
+  List,
+  SimpleForm,
+  TextField,
+  TextInput,
+  EditButton,
+  BooleanInput,
+  BooleanField,
+  Edit,
+  email,
+} from 'react-admin';
+
+export const ListUsers = () => (
+  <List>
+    <Datagrid>
+      <TextField label="ID" source="id" />
+      <TextField label="Github ID" source="githubId" />
+      <TextField label="Nombre" source="name" />
+      <TextField label="Apellido" source="lastName" />
+      <TextField label="Email (notificiaciones)" source="notificationEmail" />
+      <TextField label="Padron" source="file" />
+      <BooleanField label="Activo" source="active" />
+      <EditButton />
+    </Datagrid>
+  </List>
+);
+
+const UserFields = () => {
+  const emailValidation = email("Email invalido");
+
+  return (
+    <>
+      <TextInput required label="Nombre" source="name" />
+      <TextInput required label="Apellido" source="lastName" />
+      <TextInput label="Padron" source="file" />
+      <TextInput required label="Github ID" source="githubId" />
+      <TextInput required label="Email (notificaciones)" validate={emailValidation} source="notificationEmail" />
+    </>
+  );
+}
+
+export const CreateUser = () => (
+  <Create title="Nuevo usuario">
+    <SimpleForm>
+      <UserFields />
+    </SimpleForm>
+  </Create>
+);
+
+export const EditUser = () => {
+  return (
+    <Edit>
+      <SimpleForm>
+        <TextInput disabled source="id" />
+        <UserFields />
+        <BooleanInput label="Activo" source="active" />
+      </SimpleForm>
+    </Edit>
+  );
+}
