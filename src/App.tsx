564bd57a55603a6ac0caa396879c5a44a2147211
spenna <sebapenna19@gmail.com>
2022-12-28 20:36:53 -0300

    TH-18 Add admin users

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { ListSubjects, CreateSubject, EditSubject } from './resources/subject';
 import { ListCourses, CreateCourse, EditCourse } from './resources/course';
 import { ListUsers, CreateUser, EditUser } from './resources/user';
 import { ListRoles, CreateRole, EditRole } from './resources/role';
+import { CreateAdminUser, EditAdminUser, ListAdminUsers } from './resources/adminUser';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000/admin/graphql';
 
@@ -28,6 +29,7 @@ const App = () => {
     return (
       <Admin dataProvider={dataProvider} dashboard={Dashboard}>
         <Resource options={{ label: "Usuarios" }} name="User" list={ListUsers} create={CreateUser} edit={EditUser} />
+        <Resource options={{ label: "Usuarios (Admin)" }} name="AdminUser" list={ListAdminUsers} create={CreateAdminUser} edit={EditAdminUser} />
         <Resource options={{ label: "Materias" }} name="Subject" list={ListSubjects} create={CreateSubject} edit={EditSubject} />
         <Resource options={{ label: "Catedras" }} name="Course" list={ListCourses} create={CreateCourse} edit={EditCourse} />
         <Resource options={{ label: "Roles" }} name="Role" list={ListRoles} create={CreateRole} edit={EditRole} />
